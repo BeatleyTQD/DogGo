330fb0e8be838d5834c61297dc408874be869ff7
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-09-22 11:14:04 -0500

    added validation/UX annotations

--- DogGo/Models/Walker.cs
+++ DogGo/Models/Walker.cs
@@ -1,9 +1,13 @@
-﻿namespace DogGo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models
 {
     public class Walker
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
         public string Name { get; set; }
+        [Required]
         public int NeighborhoodId { get; set; }
         public string ImageUrl { get; set; }
         public Neighborhood Neighborhood { get; set; }
